5139efa00119f0faca1e40f2488ba723a9e2e83b
daigo-jp <daigo393969@gmail.com>
2025-06-10 23:37:05 +0900

    履歴一覧の作成、.envファイルの作成

--- myproject/arrange_message/urls.py
+++ myproject/arrange_message/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('gemini/create', views.message_input, name='input_gemini'),
     path('success/<int:pk>/', views.success, name='success'),
+    path('message_list/', views.message_list, name='message_list'),
+
 ]
